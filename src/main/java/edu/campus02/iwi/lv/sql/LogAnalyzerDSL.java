aa62baed97b9a22bd8f58d15b5e7b9083381626a
Philipp <philipp.weihs@edu.campus02.at>
2020-03-23 15:18:29 +0100

    Aufgabe2: DSL (LogFileAnalyzer).

--- src/main/java/edu/campus02/iwi/lv/sql/LogAnalyzerDSL.java
+++ src/main/java/edu/campus02/iwi/lv/sql/LogAnalyzerDSL.java
@@ -5,11 +5,9 @@ import static org.apache.spark.sql.functions.*;
 import java.util.List;
 import java.util.Objects;
 
+import edu.campus02.iwi.demo.wc.WordCountJava8RDD;
 import org.apache.spark.SparkConf;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 import spark.exercise.env.WinConfig;
 
@@ -25,26 +23,49 @@ public class LogAnalyzerDSL {
 		}
 
 		//TODO 1: create spark config & session instead of context
-		
+		SparkConf cnf = new SparkConf().setMaster("local[1]")
+				.setAppName(LogAnalyzerDSL.class.getName());
+		SparkSession spark = SparkSession.builder()
+				.config(cnf)
+				.getOrCreate();
 		
 		//TODO 2: read in the access log file with string encoder
 		//and map the elements with the bean encoder to ApacheAccessLog objects
 		//we also filter them in order to keep only non-null entries
-		
+		Dataset<ApacheAccessLog> accessLogs = spark.read().text(args[0]).as(Encoders.STRING()).
+				map(ApacheAccessLog::parseFromLogLine, Encoders.bean(ApacheAccessLog.class))
+				.filter(Objects::nonNull).cache();
 		
 		//TODO 3: inspect the schema and a few sample records
-		
+		accessLogs.printSchema();
+		accessLogs.show(10);
+		System.out.println(accessLogs.count());
+
 		//TODO 4: simple grouping by HTTP method
-		
+		RelationalGroupedDataset groupedMethods = accessLogs.groupBy("method");
+		Dataset<Row> countedGroups = groupedMethods.count();
+		countedGroups.show();
+
+
 		//TODO 5: calculate and show the top 25 requested resources (=endpoints)
+		RelationalGroupedDataset groupedEndpoints = accessLogs.groupBy("endpoint");
+		Dataset<Row> countedEndpoints = groupedEndpoints.count();
+		Dataset<Row> orderedEndpoints = countedEndpoints.orderBy(col("count").desc());
+		orderedEndpoints.show(25);
 		
 		//TODO 6: collect all hosts (=ipAddress) that have accessed
 		//the server more than 250 times into  list and it to the console
-		
+		RelationalGroupedDataset groupedHosts = accessLogs.groupBy("ipAddress");
+
+		Dataset<Row> countedHosts = groupedHosts.count();
+		Dataset<Row> filteredHosts = countedEndpoints.filter("count > 250");
+		List<Row> hostList = filteredHosts.collectAsList();
+		System.out.println(hostList);
+
 		//TODO 7: apply the aggregate functions avg, min, max, sum
 		//based on contentSize column of requested resources
 		//and show the results
-		
-		
+		accessLogs.agg(avg("contentSize"), max("contentSize"), min("contentSize"), sum("contentSize")).show();
+
 	}
 }
